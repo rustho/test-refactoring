dc11f1a1b0b9ef5cafe02dd00cb60f83eaa731cb
Mikhail Keselman <dasdgool@gmail.com>
2023-06-15 15:50:01 +0800

    update

--- dpk.js
+++ dpk.js
@@ -1,6 +1,6 @@
-import { createHash } from "crypto";
+const { createHash } = require("crypto");
 
-export const deterministicPartitionKey = (event) => {
+exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
 
   const MAX_PARTITION_KEY_LENGTH = 256;
--- dpk.test.js
+++ dpk.test.js
@@ -1,4 +1,5 @@
 const crypto = require("crypto");
+const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   let originalCreateHash;
